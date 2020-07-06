Fix colormap broadcasting

source/canvas/Colorbar/_find_colormap.py
@@ -10,7 +10,7 @@ def find_static_colormap(self, **kwargs):
 def find_colormap(self, values):
 
     if self.cbar_in_vessel:
-        values = ma.masked_array(values, mask=np.logical_not(self.run.in_vessel_mask_structured))
+        values = ma.masked_array(values, mask=np.logical_not(np.broadcast_to(self.run.in_vessel_mask_structured, values.shape)))
 
     [cbar_min, cbar_max] = self.data_limits(values)
 