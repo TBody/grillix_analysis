2414a6d00733532787c3a9954976e4ddd69ef1c4
Thomas Body <thomas.body@ipp.mpg.de>
2020-08-02 10:01:19 +0200

    Recycle memory accesses for efficiency

--- lineout.py
+++ lineout.py
@@ -42,24 +42,24 @@ from ipdb import launch_ipdb_on_exception
 
 lineout_resolution = 100
 
-def plot_lineouts(lineouts):
+def plot_projector(projector):
     flux_surface, _ = FluxSurface(run=run)()
     plt.contour(run.grid.x_unique, run.grid.y_unique, run.grid.vector_to_matrix(flux_surface.flatten()), 100)
 
-    for lineout in lineouts.values():
-        lineout.plot_sample_line()
+    projector.plot_interps()
 
     plt.title(title)
     plt.gca().set_aspect('equal')
     plt.legend()
     plt.show()
 
-def setup_results_file(results_file, run, lineouts, measurement_arrays, snap_indices, time_slice, toroidal_slice, prevent_overwrite=True):
+def setup_results_file(results_file, run, projector, measurement_array, snap_indices, time_slice, toroidal_slice, prevent_overwrite=True):
     if prevent_overwrite: assert not(results_file.exists()), f"Error: file {results_file} already exists. Manually remove before continuing"
     
+    flux_surface, _ = FluxSurface(run=run)()
+    flux_surface = run.grid.vector_to_matrix(flux_surface.flatten())
     rootgrp = Dataset(results_file, 'w')
         
-    rootgrp.createDimension("points", lineout_resolution)
     rootgrp.createDimension("time", None)
     sample_times = rootgrp.createVariable("sample_times", "f8", ("time",))
 
@@ -67,61 +67,65 @@ def setup_results_file(results_file, run, lineouts, measurement_arrays, snap_ind
     rootgrp.tau_normalisation = run.normalisation.tau_0.to('s').magnitude
     rootgrp.tau_normalisation_units = "s"
 
-    measurement_vars = {}
-    # Write in header for each lineout
-    for key, projector in lineouts.items():
+    measurement_vars = []
 
-        lineout_grp = rootgrp.createGroup(key)
-        sample_x = lineout_grp.createVariable("sample_x", "f8", ("points",))
-        sample_y = lineout_grp.createVariable("sample_y", "f8", ("points",))
-        sample_rho = lineout_grp.createVariable("sample_rho", "f8", ("points",))
-        sample_arc = lineout_grp.createVariable("sample_arc", "f8", ("points",))
+    for measurement_id in range(len(measurement_array)):
+        variable_reference = measurement_array[measurement_id].variable.title
+        variable_reference = variable_reference.replace(" ", "_").replace(".", "")
 
-        sample_x[:] = projector.x_interp
-        sample_y[:] = projector.y_interp
-        sample_rho[:] = projector.rho_values()
-        sample_arc[:] = projector.arc_l
-        lineout_grp.spatial_factor = run.normalisation.R0.magnitude
-        lineout_grp.spatial_factor_units = str(run.normalisation.R0.units)
+        var_grp = rootgrp.createGroup(variable_reference)
 
-        measurement_vars[key] = np.empty((len(measurement_arrays[key])), dtype=NCVariable)
+        normalisation_factor = measurement_array[measurement_id].variable.normalisation_factor
+        var_grp.normalisation_factor = normalisation_factor.magnitude
+        var_grp.normalisation_factor_units = str(normalisation_factor.units)
 
-        for measurement_id in range(len(measurement_arrays[key])):
-            variable_reference = measurement_arrays[key][measurement_id].variable.title
-            variable_reference = variable_reference.replace(" ", "_").replace(".", "")
+        lineout_vars = {}
 
-            var_grp = lineout_grp.createGroup(variable_reference)
-            var_grp.createVariable("values", "f8", ("time", "points"))
+        # Write in header for each lineout
+        for key, interp in projector.interps.items():
 
-            normalisation_factor = measurement_arrays[key][measurement_id].variable.normalisation_factor
-            var_grp.normalisation_factor = normalisation_factor.magnitude
-            var_grp.normalisation_factor_units = str(normalisation_factor.units)
+            lineout_grp = var_grp.createGroup(key)
+            lineout_grp.createDimension("points", interp.x_interp.size)
+            sample_x = lineout_grp.createVariable("sample_x", "f8", ("points",))
+            sample_y = lineout_grp.createVariable("sample_y", "f8", ("points",))
+            sample_rho = lineout_grp.createVariable("sample_rho", "f8", ("points",))
+            sample_arc = lineout_grp.createVariable("sample_arc", "f8", ("points",))
 
-            measurement_vars[key][measurement_id] = var_grp
-    
+            val_var = lineout_grp.createVariable("values", "f8", ("time", "points"))
+
+            sample_x[:] = interp.x_interp
+            sample_y[:] = interp.y_interp
+            sample_rho[:] = interp.rho_values(flux_surface)
+            sample_arc[:] = interp.arc_l
+            lineout_grp.spatial_factor = run.normalisation.R0.magnitude
+            lineout_grp.spatial_factor_units = str(run.normalisation.R0.units)
+
+            lineout_vars[key] = val_var
+        
+        measurement_vars.append(lineout_vars)
+
     return measurement_vars
 
-def inner_loop(snap_indices, measurement, toroidal_slice):
-    results = np.zeros((len(snap_indices), lineout_resolution))
+def fill_values(projector, measurement_array, measurement_vars, snap_indices, toroidal_slice):
 
-    for t, time in zip(range(len(snap_indices)), snap_indices):
-        print(f"\tExtracting data at {time} of [{snap_indices[0]}-{snap_indices[-1]}] for {key}, {measurement.variable.title}")
+    results = {}
+    for measurement, measurement_id in zip(measurement_array, range(len(measurement_array))):
+        for key, lineout in projector.interps.items():
+            results[key] = np.zeros((len(snap_indices), lineout.x_interp.size))
 
-        results[t, :], _ = measurement(time_slice=[time], toroidal_slice=toroidal_slice)
-    
-    return results
 
-def fill_values(lineouts, measurement_arrays, measurement_vars, snap_indices, toroidal_slice):
+        for t, time in zip(range(len(snap_indices)), snap_indices):
 
-    for key, projector in lineouts.items():
+            print(f"\tExtracting data at {time} of [{snap_indices[0]}-{snap_indices[-1]}], {measurement.variable.title}")
 
-        for measurement, measurement_id in zip(measurement_arrays[key], range(len(measurement_arrays[key]))):
+            results_at_time, _ = measurement(time_slice=[time], toroidal_slice=toroidal_slice)
             
-            var_grp = measurement_vars[key][measurement_id]
-            
-            results = inner_loop(snap_indices, measurement, toroidal_slice)
-            
-            var_grp["values"][:, :] = results
+            for key in projector.interps.keys():
+                results[key][t, :] = results_at_time[key]
+
+        for key in projector.interps.keys():
+            var_grp = measurement_vars[measurement_id][key]
+            var_grp[:, :] = results[key]
 
 if __name__=="__main__":
     # Wrapping everything with "with launch_ipdb_on_exception():" has the helpful effect that, upon a crash, the ipdb debugger is launched
@@ -150,54 +154,41 @@ if __name__=="__main__":
         operators = []
 
         # Request the different projectors
-        lineouts = {
-            # "OMP": Lineout("OMP", resolution=lineout_resolution),
-            # "IMP": Lineout("IMP", resolution=lineout_resolution),
-            "MP": Lineout("MP", resolution=lineout_resolution),
-            # "VMP": Lineout("VMP", resolution=lineout_resolution),
-            "LFS_C0": Lineout("LFS_C0", resolution=lineout_resolution),
-            # "LFS_TARGET": Lineout("LFS_TARGET", resolution=lineout_resolution),
-            "HFS_C0": Lineout("HFS_C0", resolution=lineout_resolution),
-            # "HFS_TARGET": Lineout("HFS_TARGET", resolution=lineout_resolution),
-            # "AX": Lineout("AX", resolution=lineout_resolution),
-            "AXPX": Lineout("AXPX", resolution=lineout_resolution)
-            # "AXP0": Lineout("AXP0", resolution=lineout_resolution),
-        }
+        projector = Lineout(resolution = lineout_resolution)
 
-        measurement_arrays = {}
-        print("Initialising measurement arrays")
-        for key, projector in lineouts.items():
-
-            measurement_arrays[key] = measurement_array_from_variable_array(
-                projector=projector,
-                variable_array=variables,
-                reduction=reduction,
-                operators=operators,
-                run=run)
+        measurement_array = measurement_array_from_variable_array(
+                                projector=projector,
+                                variable_array=variables,
+                                reduction=reduction,
+                                operators=operators,
+                                run=run)
         
-        # plot_lineouts(lineouts)
+        # plot_projector(projector)
+        # quit()
         
         snap_indices = run.snap_indices[time_slice]
 
-        results_file = Path(filepath)/f"lineouts_{group}_{snap_indices[0]}_{snap_indices[1]}.nc"
+        results_file = Path(filepath)/f"projector_{group}_{snap_indices[0]}_{snap_indices[-1]}.nc"
         
         if not results_file.exists():
             print(f"File {results_file} doesn't exist. Making file now.")
 
             print("Setting up results file")
             measurement_vars = setup_results_file(
-                results_file, run, lineouts, measurement_arrays, snap_indices, time_slice, toroidal_slice, prevent_overwrite=False)
+                results_file, run, projector, measurement_array, snap_indices, time_slice, toroidal_slice, prevent_overwrite=False)
 
             print("Filling results file")
-            fill_values(lineouts, measurement_arrays, measurement_vars, snap_indices, toroidal_slice)
+            fill_values(projector, measurement_array, measurement_vars, snap_indices, toroidal_slice)
 
-        # rootgrp = Dataset(results_file, 'r')
+        else:
+            print(f"File {results_file} exists. Reading from file.")
+            rootgrp = Dataset(results_file, 'r')
 
-        # plt.pcolormesh(rootgrp["MP"]["sample_rho"][np.logical_not(np.isnan(rootgrp["MP"]["sample_rho"]))],
-        #                rootgrp["sample_times"], 
-        #                rootgrp["MP"]["Density"]["values"][:,np.logical_not(np.isnan(rootgrp["MP"]["sample_rho"]))]
-        #                )
+            # plt.pcolormesh(rootgrp["Density"]["OMP"]["sample_rho"],
+            #             rootgrp["sample_times"],
+            #             rootgrp["Density"]["OMP"]["values"]
+            #             )
 
-        # plt.show()
+            plt.show()
 
 
--- source/measurements/Projector/Lineout.py
+++ source/measurements/Projector/Lineout.py
@@ -1,86 +1,124 @@
 from source import np, plt
 from . import Projector
 from ..Operator import ReduceToPoloidal
+from ...Component import Component
 from scipy import interpolate
 from source.run.Equilibrium.Numerical import NumericalEquilibrium
 from source.measurements.Variable.StaticVariable import FluxSurface
 
-class Lineout(Projector):
+class InterpolationProjection:
+
+    def plot_sample_section(self):
+        if isinstance(self, LineProjection):
+            plt.plot(self.x_interp, self.y_interp, label=self.sample_key)
+        else:
+            plt.scatter(self.x_lineout, self.y_lineout, label=self.sample_key)
     
-    def __init__(self, lineout_key, run=None, resolution=100):
+    def bilinear_interpolate(self, x, y, values):
+        assert(values.ndim == 2)
+        x_index, y_index, values = self.grid.x_unique, self.grid.y_unique, values
+
+        i = np.searchsorted(x_index, x) - 1
+        j = np.searchsorted(y_index, y) - 1
+
+        if np.any([
+                i == 0,
+                i >= values.shape[1] - 1,
+                j == 0,
+                j >= values.shape[0] - 1
+            ]):
+            # If the full stencil isn't available, return NaN (lazy handling of edge points)
+            return np.nan
+
+        x1, x2 = x_index[i:i + 2]
+        y1, y2 = y_index[j:j + 2]
+        z11, z12 = values[j][i:i + 2]
+        z21, z22 = values[j + 1][i:i + 2]
+
+        return (z11 * (x2 - x) * (y2 - y) +
+                z21 * (x - x1) * (y2 - y) +
+                z12 * (x2 - x) * (y - y1) +
+                z22 * (x - x1) * (y - y1)) / ((x2 - x1) * (y2 - y1))
+    
+    def rho_values(self, flux_surface):
+
+        return self.interpolate(flux_surface)
+    
+    def interpolate(self, shaped_values):
+        z_interp = np.zeros_like(self.x_interp)
+
+        for i, x, y in zip(range(len(z_interp)), self.x_interp, self.y_interp):
+            z_interp[i] = self.bilinear_interpolate(x, y, shaped_values)
+        
+        return z_interp
+
+class LineProjection(InterpolationProjection):
+    
+    def __init__(self, sample_key, resolution=100):
         self.resolution = resolution
-        self.lineout_key = lineout_key
-        super().__init__(run=run)
+        self.sample_key = sample_key
 
-    def request_reduction(self, reduction):
-        return reduction.cast_to_subclass(ReduceToPoloidal)
-
-    def set_run(self):
-        self.grid = self.run.grid
-        self.set_arrays(self.run)
-
-    def set_arrays(self, run):
+    def set_arrays(self, run, flux_surface):
         assert(isinstance(run.equilibrium, NumericalEquilibrium)), f"Only implemented for Numerical equilibrium"
         equi = run.equilibrium
     
-        if self.lineout_key == "OMP":
+        if self.sample_key == "OMP":
             self.x_lineout = np.linspace(1.0, run.grid.xmax)
             self.y_lineout = np.ones_like(self.x_lineout)*(equi.Z0 / equi.R0)
         
-        elif self.lineout_key == "IMP":
+        elif self.sample_key == "IMP":
             self.x_lineout = np.linspace(run.grid.xmin, 1.0)
             self.y_lineout = np.ones_like(self.x_lineout)*(equi.Z0 / equi.R0)        
         
-        elif self.lineout_key == "MP":
+        elif self.sample_key == "MP":
             self.x_lineout = np.linspace(run.grid.xmin, run.grid.xmax)
             self.y_lineout = np.ones_like(self.x_lineout)*(equi.Z0 / equi.R0)
 
-        elif self.lineout_key == "VMP":
+        elif self.sample_key == "VMP":
             self.y_lineout = np.linspace(equi.Z0/equi.R0, run.grid.ymax)
             self.x_lineout = np.ones_like(self.y_lineout)
         
-        elif self.lineout_key == "LFS_C0":
+        elif self.sample_key == "LFS_C0":
             self.x_lineout = run.penalisation_contours[0].x_arrays[1]
             self.y_lineout = run.penalisation_contours[0].y_arrays[1]
 
-        elif self.lineout_key == "LFS_TARGET":
+        elif self.sample_key == "LFS_TARGET":
             self.x_lineout = run.penalisation_contours[1].x_arrays[1]
             self.y_lineout = run.penalisation_contours[1].y_arrays[1]
 
-        elif self.lineout_key == "HFS_C0":
+        elif self.sample_key == "HFS_C0":
             self.x_lineout = run.penalisation_contours[0].x_arrays[0]
             self.y_lineout = run.penalisation_contours[0].y_arrays[0]
 
-        elif self.lineout_key == "HFS_TARGET":
+        elif self.sample_key == "HFS_TARGET":
             self.x_lineout = run.penalisation_contours[1].x_arrays[0]
             self.y_lineout = run.penalisation_contours[1].y_arrays[0]
         
-        elif self.lineout_key == "AX":
+        elif self.sample_key == "AX":
             self.x_lineout = np.linspace(run.grid.xmin, run.grid.xmax, 100)
             self.y_lineout = equi.point_axis(x=self.x_lineout, R_centre=equi.RX/equi.R0, Z_centre=equi.ZX/equi.R0)
         
-        elif self.lineout_key == "AXPX":
+        elif self.sample_key == "AXPX":
             self.x_lineout = np.linspace(run.grid.xmin, run.grid.xmax, 100)
             self.y_lineout = equi.point_axis(x=self.x_lineout, R_centre=equi.RX/equi.R0, Z_centre=equi.ZX/equi.R0, normal=True)
         
-        elif self.lineout_key == "AXP0":
+        elif self.sample_key == "AXP0":
             self.x_lineout = np.linspace(run.grid.xmin, run.grid.xmax, 100)
             self.y_lineout = equi.point_axis(x=self.x_lineout, R_centre=equi.R0/equi.R0, Z_centre=equi.Z0/equi.R0, normal=True)
             
         else:
             raise NotImplementedError(f"Lineout not recognised {lineout}")
 
-        self.x_lineout, self.y_lineout = self.remove_off_grid_points(run, self.x_lineout, self.y_lineout)
+        self.x_lineout, self.y_lineout = self.remove_off_grid_points(run, flux_surface, self.x_lineout, self.y_lineout)
 
         self.f, self.u = interpolate.splprep([self.x_lineout, self.y_lineout], s=1, per=False)
         self.x_interp, self.y_interp = interpolate.splev(np.linspace(0, 1, self.resolution), self.f)
+        self.x_interp, self.y_interp = self.remove_off_grid_points(run, flux_surface, self.x_interp, self.y_interp)
 
         self.calculate_arc_length()
-        # self.calculate_bilinear_matrix()
     
     @staticmethod
-    def remove_off_grid_points(run, x_test, y_test):
-        flux_surface, _ = FluxSurface(run=run)()
+    def remove_off_grid_points(run, flux_surface, x_test, y_test):
         flux_surface = run.grid.vector_to_matrix(flux_surface.flatten())
 
         x_test = np.array(x_test)
@@ -96,105 +134,73 @@ class Lineout(Projector):
         y_test = y_test[np.logical_not(np.isnan(z_test))]
 
         return x_test, y_test
-
-    # def calculate_bilinear_matrix(self):
-        
-    #     indi = []
-    #     indj = []
-    #     val = []
-
-    #     nz = 0
-    #     for x, y in zip(self.x_interp, self.y_interp):
-    #         indi.append(nz)
-
-    #         neighbours, weighting = self.find_neighbours(x, y)
-    
-    # def find_neighbours(self, x, y):
-        
-    #     x_vector, y_vector = self.run.x, self.run.y
-    #     x_matrix, y_matrix = self.run.x_unique, self.run.y_unique
-
-    #     if ((x > x_matrix.min()) & (x <= x_matrix.max()) &
-    #         (y > y_matrix.min()) & (y <= y_matrix.max())):
-            
-    #         i = np.searchsorted(x_matrix, x)-1
-    #         j = np.searchsorted(y_matrix, y)-1
-            
-    #         p00 = np.where(np.logical_and((x_vector == x_matrix[i]), (y_vector == y_matrix[j])))
-    #         p01 = np.where(np.logical_and((x_vector == x_matrix[i]), (y_vector == y_matrix[j+1])))
-    #         p10 = np.where(np.logical_and((x_vector == x_matrix[i+1]), (y_vector == y_matrix[j])))
-    #         p11 = np.where(np.logical_and((x_vector == x_matrix[i+1]), (y_vector == y_matrix[j+1])))
-
-    #         x1, x2 = x_index[i:i + 2]
-    #         y1, y2 = y_index[j:j + 2]
-    #         z11, z12 = vector[j][i:i + 2]
-    #         z21, z22 = values[j + 1][i:i + 2]
-
-    #     return 
-    #     (z11 * (x2 - x) * (y2 - y) +
-    #             z21 * (x - x1) * (y2 - y) +
-    #             z12 * (x2 - x) * (y - y1) +
-    #             z22 * (x - x1) * (y - y1)) / ((x2 - x1) * (y2 - y1))
     
     def calculate_arc_length(self):
 
         self.arc_l = np.cumsum(np.insert(np.sqrt(np.diff(self.x_interp) ** 2 + np.diff(self.y_interp) ** 2), 0, 0.0))
         self.arc_length = self.arc_l[-1]
+
+class Lineout(Projector):
     
+    def __init__(self, run=None, resolution=100):
+        self.resolution = resolution
+
+        self.interps = {
+            "OMP":        LineProjection("OMP",        resolution=resolution),
+            "IMP":        LineProjection("IMP",        resolution=resolution),
+            "MP":         LineProjection("MP",         resolution=resolution),
+            "VMP":        LineProjection("VMP",        resolution=resolution),
+            "LFS_C0":     LineProjection("LFS_C0",     resolution=resolution),
+            "LFS_TARGET": LineProjection("LFS_TARGET", resolution=resolution),
+            "HFS_C0":     LineProjection("HFS_C0",     resolution=resolution),
+            "HFS_TARGET": LineProjection("HFS_TARGET", resolution=resolution),
+            "AX":         LineProjection("AX",         resolution=resolution),
+            "AXPX":       LineProjection("AXPX",       resolution=resolution),
+            "AXP0":       LineProjection("AXP0",       resolution=resolution)
+        }
+
+        super().__init__(run=run)
+
+    def request_reduction(self, reduction):
+        return reduction.cast_to_subclass(ReduceToPoloidal)
+
+    def set_run(self):
+        self.grid = self.run.grid
+        flux_surface, _ = FluxSurface(run=self.run)()
+
+        for interp in self.interps.values():
+            interp.grid = self.grid
+            interp.set_arrays(self.run, flux_surface)
+
     def rho_values(self):
         flux_surface, _ = FluxSurface(run=self.run)()
 
-        return self.shape_values(flux_surface.flatten())
+        rho_vals = {}
 
-    def plot_sample_line(self):
-        # plt.scatter(self.x_lineout, self.y_lineout, label='_nolegend_')
-        plt.plot(self.x_interp, self.y_interp, label=self.lineout_key)
+        for key, interp in self.interps.items():
+            rho_vals[key] = interp.rho_values(flux_surface)
+
+        return rho_vals
     
-    @property
-    def x(self):
-        return np.linspace(0, 1, self.resolution)
-    
+    def plot_interps(self):
+
+        for interp in self.interps.values():
+            interp.plot_sample_section()
+
     def determine_slices(self, time_slice=slice(-1, None), toroidal_slice=slice(1), poloidal_slice=slice(None)):
         # Natural slicing
         # Default to use the last snap, the 0th plane, and all poloidal points
         return time_slice, toroidal_slice, poloidal_slice
 
-    def bilinear_interpolate(self, x, y, values):
-        x_index, y_index, values = self.grid.x_unique, self.grid.y_unique, values
-
-        i = np.searchsorted(x_index, x) - 1
-        j = np.searchsorted(y_index, y) - 1
-
-        if np.any([
-                i == 0,
-                i >= values.shape[1] - 1,
-                j == 0,
-                j >= values.shape[0] - 1
-            ]):
-            # If the full stencil isn't available, return NaN
-            return np.nan
-
-        x1, x2 = x_index[i:i + 2]
-        y1, y2 = y_index[j:j + 2]
-        z11, z12 = values[j][i:i + 2]
-        z21, z22 = values[j + 1][i:i + 2]
-
-        return (z11 * (x2 - x) * (y2 - y) +
-                z21 * (x - x1) * (y2 - y) +
-                z12 * (x2 - x) * (y - y1) +
-                z22 * (x - x1) * (y - y1)) / ((x2 - x1) * (y2 - y1))
-
     def shape_values(self, values):
         
         assert(values.ndim == 1)
 
-        # interpolator = interpolate.LinearNDInterpolator((self.grid.x, self.grid.y), values)
-        # z_interp = interpolator((self.x_interp, self.y_interp))
         shaped_values = self.grid.vector_to_matrix(values)
 
-        z_interp = np.zeros_like(self.x_interp)
+        z_vals = {}
 
-        for i, x, y in zip(range(len(z_interp)), self.x_interp, self.y_interp):
-            z_interp[i] = self.bilinear_interpolate(x, y, shaped_values)
+        for key, interp in self.interps.items():
+            z_vals[key] = interp.interpolate(shaped_values)
 
-        return z_interp
+        return z_vals
